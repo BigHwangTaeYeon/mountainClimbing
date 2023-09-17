70bdc39ac0135d9756a5db0eb9618e33676fe979
hwang <gxd0214@gmail.com>
2023-09-17 21:47:21 +0900

    틀 생성

---
+++ src/main/java/side/project/publicapi/controller/LoginController.java
@@ -0,0 +1,20 @@
+package side.project.publicapi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;;
+
+@RequestMapping("/login")
+@Controller
+public class LoginController {
+    
+    @RequestMapping("/connectId")
+    public String connect() {
+        return "/login/ConnectId";
+    }
+    
+    @RequestMapping("/createId")
+    public String createId() {
+        return "/login/CreateId";
+    }
+    
+}
---
+++ src/test/java/side/project/publicapi/PublicapiApplicationTests.java
@@ -0,0 +1,13 @@
+package side.project.publicapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
