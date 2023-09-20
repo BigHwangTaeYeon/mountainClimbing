5b00e3ec8e8b6e2f283b64ebe9e0f1b6e444ff84
bighwang <gxd0214@gmail.com>
2023-09-20 12:26:14 +0900

    culture API dev

--- README.md
+++ README.md
@@ -3,3 +3,17 @@
 
 # 20230919
 api 내용 등산 > 문화예술공연으로 변경
+
+//문화체육관광부_문화예술공연(통합)
+//문화체육관광부 대표 누리집의 '문화예술공연' 게시판 및 문화포털(https://www.culture.go.kr/)에 등록된 게시글을 연계하여 제공하는 공연전시 프로그램 정보로 연극, 뮤지컬, 오페라, 음악, 콘서트, 국악, 무용, 전시, 기타 등 각 분야의 문화예술공연 목록 정보를 통합 제공하고 있습니다.
+//https://www.culture.go.kr/data/openapi/openapiView.do?id=580
+
+# 20230920
+회사 gradle version
+distributionBase=GRADLE_USER_HOME
+distributionPath=wrapper/dists
+distributionUrl=https\://services.gradle.org/distributions/gradle-8.2.1-bin.zip
+networkTimeout=10000
+validateDistributionUrl=true
+zipStoreBase=GRADLE_USER_HOME
+zipStorePath=wrapper/dists
--- src/main/java/side/project/publicapi/api/api.java
+++ src/main/java/side/project/publicapi/api/api.java
@@ -5,24 +5,19 @@ import org.springframework.stereotype.Controller;;
 
 public class api {
     
-    public void cultureApi(){
+    @Value("${api.culture.key}")
+    private String cultureKey;
 
+    public void cultureApi(String method){
         String url = "http://api.kcisa.kr/openapi/CNV_060/request";
-        //문화체육관광부_문화예술공연(통합)
-        //문화체육관광부 대표 누리집의 '문화예술공연' 게시판 및 문화포털(https://www.culture.go.kr/)에 등록된 게시글을 연계하여 제공하는 공연전시 프로그램 정보로 연극, 뮤지컬, 오페라, 음악, 콘서트, 국악, 무용, 전시, 기타 등 각 분야의 문화예술공연 목록 정보를 통합 제공하고 있습니다.
-        //https://www.culture.go.kr/data/openapi/openapiView.do?id=580
-
+        String method = "GET";
         HashMap<String, String> data = new HashMap<String, String>();
-        data.push("serviceKey", "Key");
+        data.push("serviceKey", cultureKey);
         data.push("numOfRows", 10);
         data.push("pageNo", 1);
 
-        httpTestByMethod(url, "GET", data);
-		
-		httpTestByMethod(url, "POST", data);
-		
-		httpTestByMethod(url, "DELETE", data);
-
+        httpConnection hc = new httpConnection();
+        hc.httpTestByMethod(url, method, data);
     }
 
 }
